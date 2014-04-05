cca8fed38dc92131073fc639facb56f8d250e62d
Ken Howard <ken@simplicatedweb.com>
2014-04-05 14:54:52 -0700

    Added SimpleHighlight.JS plugin

---
+++ js/jquery.simplehighlight.js
@@ -0,0 +1,25 @@
+/*!
+* SimpleHighlight 1.0.0
+*
+* Copyright 2013, Ken Howard - http://kenowardpdx.com
+* Released under the WTFPL license - http://sam.zoy.org/wtfpl/
+*
+* Date: Thu Sept 27 12:00:00 2013 -0700
+*/
+(function( $ ){
+
+  "use strict";
+
+  $.simpleHighlight = function( string, terms ) {
+
+	$.each(terms, function(i,term) {
+		var regex = new RegExp('(' + term + ')\\b','gi'),
+		    newStr = '##$1&&'; // To prevent highlighting html tags
+		string = string.replace(regex, newStr);
+	});
+	string = string.replace(/##/g,'<span class="matchedTerm">').replace(/&&/g,'</span>'); // Clean up for display
+	return string;
+  };
+
+// Works with either jQuery or Zepto
+})( window.jQuery || window.Zepto );
